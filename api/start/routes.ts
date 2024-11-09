31c2e3ba6f9d4295d52939803e64c5c5a109bb79
Pablo <juanpablomadariagacardona@gmail.com>
2024-11-08 20:24:46 -0500

    fix router import adonis v6

--- api/start/routes.ts
+++ api/start/routes.ts
@@ -11,6 +11,9 @@
 
 import router from '@adonisjs/core/services/router'
 
+const ExpensesController = () => import('#controllers/expenses_controller')
+const CategoriesController = () => import('#controllers/categories_controller')
+
 // Root route to check server status
 router.get('/', async () => {
   return {
@@ -24,18 +27,18 @@ router
     // Routes for expenses operations
     router
       .group(() => {
-        router.get('/', 'expenses_controller.index') // Fetch all expenses
-        router.post('/', 'expenses_controller.store') // Create a new expense
-        router.put('/:id', 'expenses_controller.update') // Update an existing expense by ID
-        router.delete('/:id', 'expenses_controller.destroy') // Delete an expense by ID
+        router.get('/', [ExpensesController, 'index']) // Fetch all expenses
+        router.post('/', [ExpensesController, 'store']) // Create a new expense
+        router.put('/:id', [ExpensesController, 'update']) // Update an existing expense by ID
+        router.delete('/:id', [ExpensesController, 'destroy']) // Delete an expense by ID
       })
       .prefix('expenses') // Prefix for expense routes
 
     // Routes for category operations
     router
       .group(() => {
-        router.get('', 'categories_controller.index') // Fetch all categories
-        router.get('/:id', 'categories_controller.show') // Fetch a single category by ID
+        router.get('', [CategoriesController, 'index']) // Fetch all categories
+        router.get('/:id', [CategoriesController, 'show']) // Fetch a single category by ID
       })
       .prefix('categories') // Prefix for category routes
   })
